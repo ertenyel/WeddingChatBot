36bc0217d8f6008e21260217308cfa8ed46580b5
ertenyel <BogdanovKirill2000@mail.ru>
2023-04-08 15:14:21 +0300

    The first working option

---
+++ WeddingChatBot/Keyboard.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types.ReplyMarkups;
+using WeddingChatBot.DataModel;
+
+namespace WeddingChatBot
+{
+    public static class Keyboard
+    {
+        public static IReplyMarkup Get(ChatUsersContext db, User user)
+        {
+            List<List<KeyboardButton>> keyboardButtons = new List<List<KeyboardButton>>();
+            List<List<KeyboardButton>> positionButtons = GetPositionButtons(db, user);
+            List<KeyboardButton> exitButtons = GetExitButtons(db, user);
+
+            foreach (List<KeyboardButton> keyboardButton in positionButtons)
+                keyboardButtons.Add(keyboardButton);
+            if (exitButtons != null)
+                keyboardButtons.Add(exitButtons);
+
+            if (keyboardButtons.Count > 0)
+                return new ReplyKeyboardMarkup(keyboardButtons);
+            else
+                return new ReplyKeyboardRemove();
+        }
+        private static List<List<KeyboardButton>> GetPositionButtons(ChatUsersContext db, User user)
+        {
+            int? idMessage = db.GetChatPositions.Where(x => x.Id == user.IdChatPosition).Select(x => x.IdMessageText).First();
+            List<List<KeyboardButton>> keyboardButtons = new List<List<KeyboardButton>>();
+
+            if (db.GetTextsInMessage.Where(x => x.Id == idMessage && x.IdButtons != null).Count() > 0)
+            {
+                int? idButtons = db.GetTextsInMessage.Where(x => x.Id == idMessage && x.IdButtons != null).Select(x => x.IdButtons).First();
+                List<string> buttons = db.GetButtons.Where(x => x.IdMessage == idButtons).Select(x => x.Text).ToList();
+                List<KeyboardButton> tempKeyboards = new List<KeyboardButton>();
+
+                int iStartPos = 0;
+                for (int i = 0; i < buttons.Count; i++)
+                {
+                    tempKeyboards.Add(new KeyboardButton(buttons[i]));
+                    if (i == iStartPos + 1)
+                    {
+                        keyboardButtons.Add(tempKeyboards);
+                        tempKeyboards = new List<KeyboardButton>();
+                        iStartPos = i + 1;
+                    }
+
+                    if (i == buttons.Count - 1 && tempKeyboards.Count > 0)
+                        keyboardButtons.Add(tempKeyboards);
+                }
+            }
+
+            return keyboardButtons;
+        }
+        private static List<KeyboardButton> GetExitButtons(ChatUsersContext db, User user)
+        {
+            int? parentChatPosition = db.GetChatPositions.Where(x => x.Id == user.IdChatPosition).Select(x => x.ParentId).First();
+            if (parentChatPosition > 1)
+            {
+                List<string> returnsButtons = db.GetButtons.Where(x => x.ButtonType == 4 || x.ButtonType == 5).Select(x => x.Text).ToList();
+                List<KeyboardButton> tempKeyboards = new List<KeyboardButton>();
+
+                foreach (string button in returnsButtons)
+                    tempKeyboards.Add(new KeyboardButton(button));
+
+                return tempKeyboards;
+            }
+            else if (parentChatPosition == 1)
+            {
+                return new List<KeyboardButton>()
+                       {
+                           new KeyboardButton(db.GetButtons.Where(x => x.ButtonType == 5).Select(x => x.Text).First())
+                       };
+            }
+
+            return null;
+        }
+    }
+}
--- WeddingChatBot/Program.cs
+++ WeddingChatBot/Program.cs
@@ -1,37 +1,50 @@
 ﻿using System;
+using System.Data.Entity.Migrations;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
+using Telegram.Bot.Polling;
 //using Telegram.Bot.Extensions.Polling;
 using Telegram.Bot.Types;
-using Telegram.Bot.Exceptions;
-using Telegram.Bot.Polling;
-using Telegram.Bot.Types.ReplyMarkups;
-using System.Linq;
-using System.Collections.Generic;
-using WeddingChatBot.DataModel;
-using System.Data.Entity.Migrations;
-using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 using WeddingChatBot;
-using Telegram.Bot.Types.InputFiles;
-using System.IO;
-using static System.Net.Mime.MediaTypeNames;
+using WeddingChatBot.DataModel;
 
 namespace TelegramBotExperiments
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            using (ChatUsersContext chatUsers = new ChatUsersContext())
+            {
+                chatUsers.GetUsers.Where(x => x.TelegramCode == 1).Count();
+            }
+            var bot = new TelegramBotClient("6119932961:AAFQ2-JsIOnAS7-khwx8Chhu8JolUiiYnnU");
+            Console.WriteLine("Запущен бот " + bot.GetMeAsync().Result.FirstName);
+
+            var cancellationToken = new CancellationTokenSource().Token;
+            var receiverOptions = new ReceiverOptions { AllowedUpdates = { } };
+            bot.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                receiverOptions,
+                cancellationToken
+            );
+            Console.ReadLine();
+        }
         public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
-            if (update.Type == UpdateType.Message)
+            var datemes = DateTime.Now - (update.Message.Date.AddHours(3));
+            if (update.Type == UpdateType.Message && datemes.Seconds < 5)
             {
                 var message = update.Message;
                 if (!string.IsNullOrWhiteSpace(message.Text))
                 {
                     using (ChatUsersContext chatUsers = new ChatUsersContext())
                     {
-                        Console.WriteLine($"{message.From.FirstName} {message.From.LastName}: {message.Text}; {message.Date}");
                         WeddingChatBot.DataModel.User users;
                         int mainPosId = chatUsers.GetChatPositions.Where(x => x.Name == "main").Select(x => x.Id).First();
 
@@ -50,16 +63,37 @@ namespace TelegramBotExperiments
                             users = chatUsers.GetUsers.Where(x => x.TelegramCode == message.From.Id).First();
                             if (message.Text.ToLower() == "/start")
                             {
+                                Console.WriteLine($"{message.From.FirstName} {message.From.LastName}: {message.Text}; {message.Date}");
                                 users.IdChatPosition = mainPosId;
                             }
                             else if (chatUsers.GetButtons.Where(x => x.Text == message.Text).Count() > 0)
                             {
                                 int? newIdChatPosition = chatUsers.GetButtons.Where(x => x.Text == message.Text).Select(x => x.CalledChatPosition).First();
-                                if (newIdChatPosition != null)
+                                int? buttonType = chatUsers.GetButtons.Where(x => x.Text == message.Text).Select(x => x.ButtonType).First();
+                                if (buttonType == 0)
                                 {
                                     users.IdChatPosition = (int)newIdChatPosition;
                                 }
-                                //Сделать исключения с кнопками вернуться назад, вернуться в меню и кнопками для записи в таблицу. Исправить файл InitialData
+                                else if (buttonType == 4 || buttonType == 1)
+                                {
+                                    if (buttonType == 1)
+                                    {
+                                        users.Choice = message.Text;
+                                    }
+                                    users.IdChatPosition = (int)chatUsers.GetChatPositions.Where(x => x.Id == users.IdChatPosition).Select(x => x.ParentId).First();
+                                }
+                                else if (buttonType == 5)
+                                {
+                                    users.IdChatPosition = mainPosId;
+                                }
+                                else if (buttonType == 2)
+                                {
+                                    users.Alcohol = users.Alcohol + ", " + message.From.Id;
+                                }
+                                else if (buttonType == 3)
+                                {
+                                    users.Food = users.Food + ", " + message.From.Id;
+                                }
                             }
                             else
                             {
@@ -80,23 +114,8 @@ namespace TelegramBotExperiments
         {
             int? idMessage = chatUsers.GetChatPositions.Where(x => x.Id == user.IdChatPosition).Select(x => x.IdMessageText).First();
             string messageText = chatUsers.GetTextsInMessage.Where(x => x.Id == idMessage).Select(x => x.Text).First();
-            int? idButtons = chatUsers.GetTextsInMessage.Where(x => x.Id == idMessage).Select(x => x.IdButtons).First();
-            List<string> buttons = chatUsers.GetButtons.Where(x => x.IdMessage == idButtons).Select(x => x.Text).ToList();
+            IReplyMarkup replyKeyboardMarkups = Keyboard.Get(chatUsers, user);
 
-            IReplyMarkup replyKeyboardMarkups;
-            if (buttons.Count > 0)
-            {
-                List<KeyboardButton> keyboardButtons = new List<KeyboardButton>();
-
-                foreach (string button in buttons)
-                    keyboardButtons.Add(new KeyboardButton(button));
-
-                replyKeyboardMarkups = new ReplyKeyboardMarkup(keyboardButtons);
-            }
-            else
-            {
-                replyKeyboardMarkups = new ReplyKeyboardRemove();
-            }
             botClient.SendTextMessageAsync(message.Chat, messageText, replyMarkup: replyKeyboardMarkups);
 
             chatUsers.GetUsers.AddOrUpdate(user);
@@ -106,28 +125,6 @@ namespace TelegramBotExperiments
         public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
         {
             await Task.Run(() => { Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception)); });
-        }
-
-
-        static void Main(string[] args)
-        {
-            using (ChatUsersContext chatUsers = new ChatUsersContext())
-            {
-                chatUsers.GetUsers.Where(x => x.TelegramCode == 1).Count();
-            }
-            TelegramBotClient bot = new TelegramBotClient("6119932961:AAFQ2-JsIOnAS7-khwx8Chhu8JolUiiYnnU");
-            Console.WriteLine("Запущен бот " + bot.GetMeAsync().Result.FirstName);
-
-            var cts = new CancellationTokenSource();
-            var cancellationToken = cts.Token;
-            var receiverOptions = new ReceiverOptions { AllowedUpdates = { } };
-            bot.StartReceiving(
-                HandleUpdateAsync,
-                HandleErrorAsync,
-                receiverOptions,
-                cancellationToken
-            );
-            Console.ReadLine();
         }
     }
 }
